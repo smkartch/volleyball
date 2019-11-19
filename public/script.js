869101481dce892bea73fa1b098c26bcd817082e
EC2 Default User <ec2-user@ip-172-31-16-182.us-east-2.compute.internal>
2019-11-19 21:10:40 +0000

    Finished

--- public/script.js
+++ public/script.js
@@ -1,5 +1,23 @@
+/*global Vue
+global axios*/
+
 var app = new Vue({
   el: '#app',
-  data: {},
-  methods: {}
+  data: {
+    items: [],
+  },
+  methods:{
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getItems();
+    },
 });
